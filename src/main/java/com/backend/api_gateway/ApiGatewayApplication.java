0e03f077cdd6c33a0830dcf6a42abb5a7c1c732f
Valen98 <leo.wahlandtcs@gmail.com>
2025-01-27 15:34:32 +0100

    first commit

---
+++ src/main/java/com/backend/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,58 @@
+package com.backend.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@SpringBootApplication
+@RestController
+public class ApiGatewayApplication {
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173")); // Allow specific origin
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // Include OPTIONS
+		configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization")); // Allow headers
+		configuration.setAllowCredentials(true); // Allow credentials (if needed)
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return new CorsWebFilter(source);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder, AuthenticationFilter authenticationFilter) {
+		return builder.routes()
+				.route(p -> p.
+						path("/auth/**").uri("http://localhost:5000"))
+				.route(p -> p.path("/users").uri("http://localhost:5000"))
+				.route(p -> p
+						.path("/profile/**")
+						.filters(f -> f.filter(authenticationFilter))
+						.uri("http://localhost:5001"))
+				.build();
+	}
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
+	}
+
+}
---
+++ src/main/java/com/backend/api_gateway/AuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.backend.api_gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+
+@Component
+public class AuthenticationFilter implements GatewayFilter {
+
+    private final WebClient webClient;
+
+    private String userServiceUrl = "http://localhost:5000";
+
+    public AuthenticationFilter(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if(authHeader == null || !authHeader.startsWith("Bearer")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+
+        return webClient.get()
+                    .uri(userServiceUrl + "/auth/validate")
+                .header(HttpHeaders.AUTHORIZATION, authHeader)
+                .retrieve()
+                .toBodilessEntity()
+                .flatMap(response -> {
+                    // If validation is successful, proceed with the request
+                    if (response.getStatusCode() == HttpStatus.OK) {
+                        return chain.filter(exchange);
+                    }
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                })
+                .onErrorResume(e -> {
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                });
+    }
+
+
+
+}
